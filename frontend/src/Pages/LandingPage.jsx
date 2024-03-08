6056f20decb9c953179dafd22693ffc0944d741d
sunilkumar8847 <sonu8847838585@gmail.com>
2024-03-08 11:47:03 +0530

    App Name change

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
 
     return (
         <div className="container mx-auto px-4 py-8 bg-purple-200">
-            <h1 className="text-3xl font-bold text-center mb-8">Welcome to <b className='text-purple-700'>PayPulse</b></h1>
+            <h1 className="text-3xl font-bold text-center mb-8">Welcome to <b className='text-purple-700'>PayPluse</b></h1>
             <div className="flex justify-center gap-40 mx-80 px-40">
 
                 <Button lebel={"Signup"} onClick={() => { navigate("/signup") }} className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-4' />
