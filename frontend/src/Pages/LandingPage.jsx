71d0852b9487e6aa15861d21bff1d13ac1bcbd96
sunilkumar8847 <sonu8847838585@gmail.com>
2024-03-09 13:08:30 +0530

    style changed

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -2,18 +2,15 @@ import React from 'react';
 import Button from '../Components/Button';
 import { useNavigate } from 'react-router-dom';
 
-
 const LandingPage = () => {
     const navigate = useNavigate();
 
-
     return (
-        <div className="container mx-auto px-4 py-8 bg-purple-200">
+        <div className="container mx-auto px-4 py-4 bg-purple-200 py-48 sm:py-60">
             <h1 className="text-3xl font-bold text-center mb-8">Welcome to <b className='text-purple-700'>PayPluse</b></h1>
-            <div className="flex justify-center gap-40 mx-80 px-40">
-
-                <Button lebel={"Signup"} onClick={() => { navigate("/signup") }} className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-4' />
-                <Button lebel={"Signin"} onClick={() => { navigate("/signin") }} className='bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded' />
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 justify-center mt-32">
+                <Button onClick={() => navigate("/signup")} lebel={"SignUp"} />
+                <Button onClick={() => navigate("/signin")} lebel={"SignIn"} />
             </div>
         </div>
     );
