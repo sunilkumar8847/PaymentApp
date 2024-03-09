ac60e61116ed00c1e6e2c2c803e89be4cda1f64b
Sunil Kumar Sahoo <sonu8847838585@gmail.com>
2024-03-09 13:41:53 +0530

    Update LandingPage.jsx

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -7,7 +7,7 @@ const LandingPage = () => {
 
     return (
         <div className="container mx-auto px-4 py-4 bg-purple-200 py-48 sm:py-60">
-            <h1 className="text-3xl font-bold text-center mb-8">Welcome to <b className='text-purple-700'>PayPluse</b></h1>
+            <h1 className="text-3xl font-bold text-center mb-8">Welcome to <b className='text-purple-700'>PayPlus</b></h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 justify-center mt-32">
                 <Button onClick={() => navigate("/signup")} lebel={"SignUp"} />
                 <Button onClick={() => navigate("/signin")} lebel={"SignIn"} />
